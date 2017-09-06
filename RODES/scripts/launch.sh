1dc5d242f504d215f864a8b1f02c33178dc65873
Simos Gerasimou <simos@cs.york.ac.uk>
2017-09-06 10:14:45 +0100

    - updated launch script

--- RODES/scripts/launch.sh
+++ RODES/scripts/launch.sh
@@ -14,8 +14,23 @@ fi
 #For launching an instance only
 PRISM_DIR=repo/prism
 
-export DYLLD_LIBRARY_PATH="$PRISM_DIR":$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH="$PRISM_DIR":$DYLD_LIBRARY_PATH
 
 vmArgs="-Xmx3g -XX:ParallelGCThreads=1"
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux
+    				export LD_LIBRARY_PATH="$PRISM_DIR":$LD_LIBRARY_PATH
+    				;;
+    Darwin*)    machine=Mac
+    				export DYLD_LIBRARY_PATH="$PRISM_DIR":$DYLD_LIBRARY_PATH
+    				;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+
 java $vmArgs -jar RODES.jar
+
